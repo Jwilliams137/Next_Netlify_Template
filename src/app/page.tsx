630806c767fc9ab0f9768c1b5434d29f94fab7d2
Julie Williams <jwilliams137.036@gmail.com>
2025-02-21 08:50:04 -0500

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       It's renovated for easy deployment to Netlify.</h2>
       <p className={styles.instructions}>To get started you will need to run npm install and then npm run dev</p>
       <p className={styles.instructions}>&#8226; It also has a contact page setup for free use with Formsubmit.
-        All you need to do to get the contact form working is add your email address to the env.local.
+        All you need to do to get the contact form working is add your email address to the env.local
         The redirect after the form is submitted is currently setup for http://localhost:3000/thank-you</p>
       <p className={styles.instructions}>&#8226; To change the redirect after form submission you will need to edit this value in the api/contact/route.js
         and also in the app/contact/page.js</p>

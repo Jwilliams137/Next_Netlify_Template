440e3b44d2d7733d29abeb2c09f8e21e7127b86b
Julie Williams <jwilliams137.036@gmail.com>
2025-03-08 11:14:08 -0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,23 @@ import Footer from '../components/Footer/Footer';
 import UnderConstruction from '../components/UnderConstruction/UnderConstruction'
 
 export const metadata: Metadata = {
-  title: "Nextlify Template",
-  description: "Next.js template renovated for easy deployment on Netlify including responsive navigation and footer ",
+  title: "Nextlify Starter Template",
+  description: "Next.js template renovated for easy deployment on Netlify including responsive navigation and footer",
+  openGraph: {
+    title: "Nextlify Starter",
+    description: "Next.js template renovated for easy deployment on Netlify including responsive navigation and footer",
+    url: "https://nextlify-starter.netlify.app",
+    siteName: "Nextlify",
+    images: [
+      {
+        url: "https://nextlify-starter.netlify.app/NextlifySharing.jpg",
+        width: 1200,
+        height: 630,
+        alt: "A starter template for Next.js ready for deployment on Netlify",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

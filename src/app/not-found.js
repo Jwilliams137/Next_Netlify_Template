80eaf3b6e47210cbc8842ea41381da19bca1515e
Julie Williams <jwilliams137.036@gmail.com>
2025-03-08 16:38:25 -0500

    unescaped

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,9 +6,9 @@ export default function NotFound() {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>404 - Page Not Found</h2>
-      <p className={styles.text}>Oops! We couldn't find that page.</p>
-      <p className={styles.text}>There are several pages in the navigation that don't exist beause this is a template</p>
-      <p className={styles.text}>Please create the pages you need then add them by modifying the navData.json</p>
+      <p className={styles.text}>Oops! We couldn&apos;t find that page.</p>
+      <p className={styles.text}>There are several pages in the navigation that don&apos;t exist because this is a template.</p>
+      <p className={styles.text}>Please create the pages you need, then add them by modifying the navData.json.</p>
       <Link href="/" className={styles.button}>Go Home</Link>
     </div>
   );
